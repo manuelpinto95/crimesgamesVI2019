c5111466b998b121ccf3afe845cd0209a7116dbd
FiroGX <firogx@gmail.com>
2019-12-12 13:31:29 +0000

    world cloud adpatation to support years and states

--- wordCloud.js
+++ wordCloud.js
@@ -1,33 +1,27 @@
 //------------------------
 
-var cloudWords = [{word: "a", size: 20}];
+var wordCloud = {
+    svg : 0,
+    words: []
+};
+
+
+wordCloud.words = [{word: "A", size: 20}];
 d3.csv("/data/ms/MSWords.csv").then(function (data) {
 
     data.forEach(function (d) {
-        cloudWords.push({word: d.Word, size: 20});
-        /*
-        if(d.count < 10 )
-            cloudWords.push({word: d.Word, size: 10});
-        else
-            if(d.cont < 20)
-                cloudWords.push({word: d.Word, size: 20});
-            else
-                if(d.cont < 30)
-                    cloudWords.push({word: d.Word, size: 30});
-                else
-                    cloudWords.push({word: d.Word, size: 40});*/
-                    
-        //console.log(d.Word);
+        wordCloud.words.push({word: d.Word, size: 20});
+        
     });
 
     gen_WordCloud();
 });
 
 function gen_WordCloud() {
-    console.log("cloud");
+    console.log("gen wordCloud");
     
     // List of words
-    var myWords = cloudWords; //[{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ]
+    var myWords = wordCloud.words;
 
     // set the dimensions and margins of the graph
     var margin = {
@@ -41,7 +35,7 @@ function gen_WordCloud() {
         width = window.innerWidth - mapW - 500 - margin.right - margin.left;
 
     // append the svg object to the body of the page
-    var svg = d3.select("#wordcloud").append("svg")
+    wordCloud.svg = d3.select("#wordcloud").append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
@@ -71,11 +65,11 @@ function gen_WordCloud() {
     // This function takes the output of 'layout' above and draw the words
     // Wordcloud features that are THE SAME from one word to the other can be here
     function draw(words) {
-        svg
+        wordCloud.svg
             .append("g")
             .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
             .selectAll("text")
-            .data(words)
+            .data(wordCloud.words)
             .enter().append("text")
             .style("font-size", function (d) {
                 return d.size;
@@ -91,5 +85,10 @@ function gen_WordCloud() {
             });
     }
 }
+/*
+update_wordCloud(){
+    wordCloud.svg.remove();
+    gen_WordCloud();
+}*/
 
 
